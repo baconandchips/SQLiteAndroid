c2b1e843b41dae36849dc5d62a4cb21637d18472
Guanting <16443280+baconandchips@users.noreply.github.com>
2021-04-25 23:37:03 -0700

    connect addOne to app

--- app/src/main/java/com/example/sqlite_android/MainActivity.java
+++ app/src/main/java/com/example/sqlite_android/MainActivity.java
@@ -35,17 +35,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                CustomerModel customerModel;
+
                 try {
-                    CustomerModel customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+                    customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
                     Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
                 }
                 catch(Exception e) {
                     Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1, "error", 0, false); // Arbitrary values
                 }
 
                 DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
+                boolean success = dataBaseHelper.addOne(customerModel);
 
+                Toast.makeText(MainActivity.this, "Success = " + success, Toast.LENGTH_SHORT).show();
 
             }
         });
